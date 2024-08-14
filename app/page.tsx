01fa6cea8edcbc2ea165598f0bf0337d2f756b89
Prantaneel Pegu <prantaneel.p@gmail.com>
2024-08-14 22:18:27 +0530

    Configured Basic Settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,11 @@
-export default function Home() {}
+"use client";
+
+export default function Home() {
+    return (
+        <main>
+            <h1 className="mt-24 text-center text-4xl text-yellow-400">
+                FinCo | A Fully-Featured Personal Finance WebApp
+            </h1>
+        </main>
+    );
+}
